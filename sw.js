f8686af9a90633c867c2c2832a96a5eedb4ec586
Ali-Good-Gaming <emailforsubscribersali.gg@googlemail.com>
2025-08-02 17:16:58 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
-const CACHE_NAME = 'studcalc-v1';
+const CACHE_NAME = 'studcalc-v3';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/style.css',
-  '/app.js',
-  '/icon-192x192.png',
-  '/icon-512x512.png'
+  '/manifest.json',
+  // Добавьте сюда пути ко всем необходимым файлам
+  '/img/emojis/party.gif',
+  '/img/emojis/happy.gif',
+  '/img/emojis/neutral.gif',
+  '/img/emojis/sad.gif'
 ];
 
 self.addEventListener('install', event => {
@@ -19,5 +21,20 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
